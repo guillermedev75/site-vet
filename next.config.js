7eb592ddd375ea050e0c2cf144c336e1befb6530
guillermedev75 <gui.manhaes75@gmail.com>
2023-10-14 03:48:59 -0300

    fazendo a porra do header denovo

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+module.exports = {
+    generateBuildId: async () => {
+      // You can, for example, get the latest git commit hash here
+      return 'my-build-id'
+    },
+  }
---
+++ src/app/contato/page.js
@@ -0,0 +1,10 @@
+import Header from '../componente/header/header'
+
+export default function Home() {
+  return (
+    <>
+        <Header />
+        <h1>Contato</h1>
+    </>
+  )
+}
