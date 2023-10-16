66a983e4a857a3fe8e94fa38191583aee802227c
guillermedev75 <gui.manhaes75@gmail.com>
2023-10-16 13:19:39 -0300

    design lines e profile container

---
+++ src/app/componente/design_lines/design_lines.js
@@ -0,0 +1,19 @@
+'use client';
+
+//Bibliotecas
+
+//Folhas de estilo
+import './style.css'
+
+export default function profile_card(props) {
+
+    return (
+        <div className='container_lines'>
+            <h3 className='container_text'>Nossa Familia</h3>
+            <div className='white_line'></div>
+            <div className='green_line'>
+                <div className='square_line'></div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Header from './componente/header/header'
 import Banner from './componente/start-banner/start-banner'
 import ProfileContainer from './componente/profile_container/profile_container'
+import DesignLines from './componente/design_lines/design_lines'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <DesignLines />
       <ProfileContainer/>
     </>
   )
